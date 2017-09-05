eca78479a945ae1dd7d968c543059f6260f60aac
RENATO RIBEIRO DA SILVA <rrs17@inf.ufpr.br>
2017-09-05 16:47:28 -0300

    periodo 1 completo

---
+++ periodo-1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lcrandom.h"
+
+int periodo[10000000],aux;
+
+
+void calculoperiodo(){
+	int aux=0,sair=0,cont=0;
+
+	for (int i = 0; i < 10000000; ++i)
+	{
+		periodo[i]=0;
+	}
+
+	while (sair==0){
+		aux=lcrandom();
+		periodo[aux]++;
+		if (periodo[aux]>1)
+		{
+			sair=1;
+			printf("%d\n",cont);
+		} 
+		cont++;
+	}
+
+
+}
+
+
+int main()
+{
+	//lcrandom_parms(1103515245,12345,2147483648);
+	calculoperiodo();
+	return 0;
+}
